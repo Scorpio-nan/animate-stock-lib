0aae58818168176c8203539b35bce234f226d547
nanxiangyuan <407001826@qq.com>
2017-12-06 12:43:37 +0800

    no message

--- script-tool/tool.js
+++ script-tool/tool.js
@@ -542,6 +542,23 @@ var tool = {
 				upCb();
 			}
 		});
+	},
+
+	//判断当前设备是手持设备还是客户端	 手持设备返回 0 ，客户端返回 1
+	browserRedirect：function(){
+		var sUserAgent = navigator.userAgent.toLowerCase();
+		var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+		var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+		var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+		var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+		var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+		var bIsAndroid = sUserAgent.match(/android/i) == "android";
+		var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+		var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+		if(!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM)) {
+			return 1;
+		}
+		return 0;
 	}
 
 };

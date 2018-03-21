aa63d927db439bd2b7e1a57821f9443b21a170b8
nanxiangyuan <407001826@qq.com>
2018-03-21 15:35:41 +0800

    新增代码

--- script-tool/tool.js
+++ script-tool/tool.js
@@ -591,6 +591,22 @@ var tool = {
 		}
 	},
 
+	//随机生成数字和字母的组合
+	randomWord:function(randomFlag, min, max){
+		var str = "",
+        range = min,
+        arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+		// 随机产生
+		if(randomFlag){
+			range = Math.round(Math.random() * (max-min)) + min;
+		}
+		for(var i=0; i<range; i++){
+			var pos = Math.round(Math.random() * (arr.length-1));
+			str += arr[pos];
+		}
+		return str;
+	},
+
 	
 	
 

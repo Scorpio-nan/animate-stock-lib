b8b37beb755753083a98b9e64c106deff9d5e895
caogen_hope <374505085@qq.com>
2020-12-21 11:21:25 +0800

    新增ts 工具方法

--- Typescript/tool.ts
+++ Typescript/tool.ts
@@ -44,3 +44,129 @@ class Tool{
     }
 	
 }
+
+
+
+
+export function DateFormat(fmt: string = "yyyy-MM-dd hh:mm:ss"): string {
+    var d: Date = new Date();
+    let o: any = {
+        "M+": d.getMonth() + 1,
+        "d+": d.getDate(),
+        "h+": d.getHours(),
+        "m+": d.getMinutes(),
+        "s+": d.getSeconds(),
+        "q+": Math.floor((d.getMonth() + 3) / 3),
+        "S": d.getMilliseconds()
+    }
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (d.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    for (let k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+    return fmt;
+}
+
+/**
+ * 获取几天前的日期
+ */
+export function getDateBefore(n:number):string{
+    var dd:Date = new Date();
+    dd.setDate(dd.getDate() + n);
+    var y = dd.getFullYear();  
+	var m = (dd.getMonth()+1)<10 ? ('0'+(dd.getMonth()+1)) : (dd.getMonth()+1);  
+    var d = dd.getDate() <10 ? ('0'+ dd.getDate()) :dd.getDate();  
+    return y+"-"+m+"-"+d;
+}
+
+interface t {
+    start_time:string
+    end_time:string
+}
+
+export enum timeSpan {
+    today = "today",
+    yestaday = "yestaday",
+    lasetweek = "lasetweek",
+    week = "week",
+    lastmonth = "lastmonth",
+    month = "month",
+    all = "all"
+}
+
+export function getDateSpan(action:string):t{
+    var start,end;
+    var d:Date = new Date();
+    switch(action){
+        case "today":
+            start = getDateBefore(0) + "00:00:00";
+            end = getDateBefore(0) + "23:59:59";
+            break;
+        case "yestaday":
+            start = getDateBefore(1) + " 00:00:00";
+			end = getDateBefore(1) + " 23:59:59";
+            break;
+        case "lasetweek":
+            start = getDateBefore(7) + " 00:00:00";
+            end = getDateBefore(0) + " 23:59:59";
+            break;
+        case "week":
+            start = getDateBefore(d.getDay()) + " 00:00:00";
+            end = getDateBefore(0) + " 23:59:59";
+            break;
+        case "lastmonth":
+            start = getDateBefore(30) + " 00:00:00";
+            end = getDateBefore(0) + " 23:59:59";
+            break;
+        case "month":
+            start = getDateBefore(d.getDate()) + " 00:00:00";
+            end = getDateBefore(0) + " 23:59:59";
+            break;
+        case "all":
+            start = "2017-01-01 00:00:00";
+            end = getDateBefore(0) + " 23:59:59";
+            break;
+    }
+    return {
+        start_time:start,
+        end_time:start
+    }
+}
+
+let obj = {};
+const $on = (name:string,fn:Function)=>{
+    if(!obj[name]){
+        obj[name] = [];
+    }
+    obj[name].push(fn);
+}
+
+const $emit = (name:string,val:any = null)=>{
+    if(obj[name]){
+        obj[name].map((fn)=>{
+            fn(val);
+        });
+    }
+}
+
+const $off = (name:string,fn:Function)=>{
+    if(obj[name]){
+        if(fn){
+            let index = obj[name].indexOf(fn);
+            if(index > -1){
+                obj[name].splice(index,1);
+            }           
+        }else{
+            obj[name].length = 0;
+        }  
+    }
+}
+
+export const Event = {
+    $on,
+    $emit,
+    $off
+}
